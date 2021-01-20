f07d83cf57b099382b7f598d462de69bbfa181a3
obtusa07 <obtusa07@gmail.com>
2021-01-20 22:22:18 +0900

    ~p115

---
+++ src/main/java/com/example/moat/springboot/domain/posts/Posts.java
@@ -0,0 +1,42 @@
+package com.example.moat.springboot.domain.posts;
+
+
+import lombok.NoArgsConstructor;
+import lombok.Getter;
+import lombok.Builder;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(columnDefinition = "TEXT", nullable = false)
+    private String content;
+
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author) {
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
+}
---
+++ src/main/java/com/example/moat/springboot/web/dto/PostsSaveRequestDto.java
@@ -0,0 +1,29 @@
+package com.example.moat.springboot.web.dto;
+
+import com.example.moat.springboot.domain.posts.Posts;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostsSaveRequestDto {
+    public String title;
+    public String content;
+    public String author;
+
+    @Builder
+    public PostsSaveRequestDto(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+    public Posts toEntity(){
+        return Posts.builder()
+                .title(title)
+                .content(content)
+                .author(author)
+                .build();
+    }
+}
