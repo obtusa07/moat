e853514feb28b3df76925c2a79915c2004d50e52
obtusa07 <obtusa07@gmail.com>
2021-01-23 19:48:16 +0900

    ~p161 CRUD 완성

--- src/main/resources/static/js/app/index.js
+++ src/main/resources/static/js/app/index.js
@@ -7,7 +7,11 @@ var main = {
         $('#btn-update').on('click', function () {
             _this.update();
         });
+        $('#btn-delete').on('click', function () {
+            _this.delete();
+        });
     },
+
     save: function () {
         var data = {
             title: $('#title').val(),
@@ -28,6 +32,7 @@ var main = {
             alert(JSON.stringify(error));
         });
     },
+
     update : function () {
         var data = {
             title: $('#title').val(),
@@ -44,6 +49,22 @@ var main = {
             data: JSON.stringify(data)
         }).done(function() {
             alert('글이 수정되었습니다.');
+            window.location.href = '/';
+        }).fail(function (error) {
+            alert(JSON.stringify(error));
+        });
+    },
+
+    delete : function () {
+        var id = $('#id').val();
+
+        $.ajax({
+            type: 'DELETE',
+            url: '/api/v1/posts/'+id,
+            dataType: 'json',
+            contentType:'application/json; charset=utf-8'
+        }).done(function() {
+            alert('글이 삭제되었습니다.');
             window.location.href = '/';
         }).fail(function (error) {
             alert(JSON.stringify(error));
